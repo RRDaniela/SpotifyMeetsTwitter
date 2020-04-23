cf5dabb15b70b24c3ce6b5a907805f0d23a4a342
rrdaniela <fosterdragonfighters@gmail.com>
2020-04-23 17:42:51 -0500

    Ver canción, artista y id del artista. NO HAY REFRESH TOKEN TODAVÍA.

--- part1.py
+++ part1.py
@@ -3,7 +3,7 @@ import json
 
 #Configuración
 urlReproduccionReciente="https://api.spotify.com/v1/me/player/currently-playing"
-token="BQDpAYyZjorYEdsudMqzDsX5h_Ssz_Jv-Iyqk9CgQhOXfDEuiKeimmXbfhaJtpjVqXh0XaHCYvygqb58fIErhUcu1wVMU2_OvFDJKmMORByjurO2hQV08PyjzYnKEHMndJgzX3B4CZClPwdBuUmE_kbSNwhj-MU1yswyUgMz_bZAZrgb"
+token="BQDAJHoQs-53W7x7KafUMIYekg2kuOT7Wu0Me21yM5ws8pWUmMLioGrlnCOO3aXbUo6VosiRYZWNbQy2mokY1g-kuqOYAvv8g-u2H4YISHxuCjY19j0BrMgnxruQJA418T8fxdyvUEwdr6T8Z_5rXTIJDwdVYTKJEePgjaF7zZwsQrK1"
 user_id="1280778938?si=6cHOzXEEQDWR9udOsXGdOg"
 
 query=f'{urlReproduccionReciente}'
@@ -15,39 +15,9 @@ response=requests.get(query,
 
 respuesta=response.json()
 
-print(respuesta['item']['name'])
-#cancion = respuesta['item']['name']
-#artista = respuesta['item']['artists'][0]['name']
-#id_Artista = respuesta['item']['artists'][0]['id']
+cancion = respuesta['item']['name']
+artista = respuesta['item']['artists'][0]['name']
+id_Artista = respuesta['item']['artists'][0]['id']
 
-#print(f"Cancion: {cancion} Artista: {artista} Id: {id_Artista}")
+print(f"Cancion: {cancion} Artista: {artista} Id: {id_Artista}")
 
-'''
-# SETTINGS 
-endpoint_url = "https://api.spotify.com/v1/recommendations?"
-token = "BQBwSnbMO2bUtZ8mJ07bddrUBkC_XlOek78vPutjX_6xCcGCnqo5L4v7Qqy6s0xLnrT9gGJ5n1MO1WiewU4ekwJQ_irZshWEMuWQOljLPZqsD0UZACceCkqky2B40IE4N4M_tDl3syabwNVa_SHfGA9Jk4eBYpLluAFoLJrgB4w"
-user_id = "1280778938?si=6cHOzXEEQDWR9udOsXGdOg"
-
-
-# OUR FILTERS
-limit=10
-market="US"
-seed_genres="indie"
-target_danceability=0.5
-uris = [] 
-
-# PERFORM THE QUERY
-query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}'
-
-
-response = requests.get(query, 
-               headers={"Content-Type":"application/json", 
-                        "Authorization":f"Bearer {token}"})
-json_response = response.json()
-
-json_response = response.json()
-
-for i in json_response['tracks']:
-            uris.append(i)
-            print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
-'''
